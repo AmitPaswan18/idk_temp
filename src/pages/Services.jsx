e398bd91e6dffcf3453e010ce44b47e764d494ba
Amit Paswan <ak9425391@gmail.com>
2024-03-22 13:13:59 +0530

    file added

---
+++ src/pages/Services.jsx
@@ -0,0 +1,33 @@
+// Filename - pages/Services.js
+
+export const Services = () => {
+  return (
+    <div className="services">
+      <h1>GeeksforGeeks Services</h1>
+    </div>
+  );
+};
+
+export const ServicesOne = () => {
+  return (
+    <div className="services">
+      <h1>GeeksforGeeks Service1</h1>
+    </div>
+  );
+};
+
+export const ServicesTwo = () => {
+  return (
+    <div className="services">
+      <h1>GeeksforGeeks Service2</h1>
+    </div>
+  );
+};
+
+export const ServicesThree = () => {
+  return (
+    <div className="services">
+      <h1>GeeksforGeeks Service3</h1>
+    </div>
+  );
+};
